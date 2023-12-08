5b19b18c3b4aa33bb20f91fa7a2ae9afd7687ca9
saboorbakshi <saboorbak@icloud.com>
2023-12-08 02:49:24 -0500

    completed day 1 part 1

---
+++ day01/part01.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    ifstream inputFile("input.txt");
+
+    if (!inputFile.is_open()) {
+        cerr << "Error opening the file!" << endl;
+        return 1;
+    }
+
+    int sum = 0;
+    string line, c;
+
+    while (getline(inputFile, line)) {
+
+        int len = line.length();
+
+        for (int i = 0; i < len; ++i) {
+            if (isdigit(line[i])) {
+                c = line[i];               
+                break;
+            }
+        }
+        
+        for (int i = len - 1; i >= 0; --i) {
+            if (isdigit(line[i])) {
+                c += line[i];             
+                break;
+            }
+        }
+
+        int x = stoi(c);
+
+        cout << x << endl;
+
+        sum += x;
+    }
+
+    inputFile.close();
+
+    cout << sum << endl;
+
+    return 0;
+}
